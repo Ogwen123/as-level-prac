10a43980d6b24d4cf45f7fbe3b420f0f8e7b03b9
Ogwen123 <owen.d.jones@btinternet.com>
2022-10-02 17:14:08 +0100

    made it so you can edit defintions

--- Section B/Tkinter/Lesson 2/lesson2activity3.py
+++ Section B/Tkinter/Lesson 2/lesson2activity3.py
@@ -8,15 +8,23 @@ window.title("Dictionary Program")
 
 dictionary = {}
 script_dir = os.path.dirname(os.path.abspath(__file__))#gets the current directory of the script
+current_word = ""
 
 with open(script_dir + "./definitions.json", "r") as read_file:
     dictionary = json.load(read_file)
     read_file.close()
 
 #this functions is run when the button is pressed
-def button_click():
+def search_dict():
     definition = ""
     entry_text = entry1.get()
+    global current_word
+
+    if defintion_textbox["state"] == "normal":
+        change_search_status_label("Save your definition changes before searching!", "red")
+        return
+
+    current_word = entry_text
 
     if not entry_text:
         change_search_status_label("You must enter a value!", "red")
@@ -30,10 +38,10 @@ def button_click():
         change_search_status_label("This value is not in the dictionary!", "red")
         return
     global text
-    text1.config(state="normal")
-    text1.delete(0.0, END)
-    text1.insert(END, definition)
-    text1.config(state="disabled")
+    defintion_textbox.config(state="normal")
+    defintion_textbox.delete(0.0, END)
+    defintion_textbox.insert(END, definition)
+    defintion_textbox.config(state="disabled")
 
 def add_key_value():
     def clear_add_entry():
@@ -91,6 +99,33 @@ def delete_key_value():
     
     change_delete_status_label("Deleted Word", "green")
 
+def save_textbox():
+    dictionary[current_word] = defintion_textbox.get(0.0, END)
+        
+    with open(script_dir + "./definitions.json", "w") as write_file:
+        json.dump(dictionary, write_file)
+        write_file.close()
+    
+    change_edit_definition_status_label("Saved new definition!", "green")
+
+def edit_definition():
+    if not current_word:
+        change_edit_definition_status_label("You must have a definition to edit!", "red")
+        return
+
+    if defintion_textbox["state"] == "disabled":#making the box editable
+        defintion_textbox.config(state="normal")
+        edit_button.config(text="Save")
+        edit_button.config(bg="light yellow")
+
+    elif defintion_textbox["state"] == "normal":#making the box uneditable and saving the new defintion
+        defintion_textbox.config(state="disabled")
+        edit_button.config(text="Edit")
+        edit_button.config(bg="light blue")
+
+        save_textbox()
+
+
 #change error labels
 def change_search_status_label(error_text, colour):
     search_status_label.config(fg=colour)#changes the text colour of the label
@@ -107,25 +142,36 @@ def change_delete_status_label(error_text, colour):
     delete_status_label.config(text=error_text)
     delete_status_label.after(4000, lambda: delete_status_label.config(text=""))
 
+def change_edit_definition_status_label(error_text, colour):
+    edit_definition_status_label.config(fg=colour)
+    edit_definition_status_label.config(text=error_text)
+    edit_definition_status_label.after(4000, lambda: edit_definition_status_label.config(text=""))
+
 
 #use the dictionary
 label1 = Label(window, text="Enter your word here: ")
 label1.grid(row=0, column=0)
 
 entry1 = Entry(window)
-entry1.grid(row=1, column=0, pady=5, padx=35, sticky=W)
+entry1.grid(row=1, column=0, pady=5, padx=(65, 0), sticky=W)
 
-button1 = Button(window, text="Enter", command=button_click, bg="#A2E9FF")
-button1.grid(row=1, column=0, pady=5, padx=35, sticky=E)
+enter_button = Button(window, text="Enter", command=search_dict, bg="light blue")
+enter_button.grid(row=1, column=0, pady=5, padx=(0, 65), sticky=E)
 
-search_status_label = Label(window, text="", fg="red")
+search_status_label = Label(window, text="")
 search_status_label.grid(row=2, column=0)
 
 label2 = Label(window, text="Definition: ")
 label2.grid(row=3, column=0, sticky=S, padx=20)
 
-text1 = Text(window, width="25", height="9", wrap=WORD)
-text1.grid(row=4, column=0, rowspan=2, pady=10, padx=20)
+defintion_textbox = Text(window, width="35", height="15", wrap=WORD, state="disabled")
+defintion_textbox.grid(row=4, column=0, rowspan=2, pady=10, padx=20)
+
+edit_button = Button(window, text="Edit", bg="light blue",command=edit_definition)
+edit_button.grid(row=6, column=0, sticky=W, padx=20, pady=(5, 10))
+
+edit_definition_status_label = Label(window, text="")
+edit_definition_status_label.grid(row=6, column=0, sticky=E, padx=20)
 
 #add key/value to the dict
 label3 = Label(window, text="Enter Word: ")
@@ -143,7 +189,7 @@ definition_entry.grid(row=3, column=1)
 add_button = Button(window, text="Add Definition", command=add_key_value, bg="#87AE73")
 add_button.grid(row=4, column=1, sticky=N, pady=5)
 
-add_status_label = Label(window, text="", fg="red")
+add_status_label = Label(window, text="")
 add_status_label.grid(row=4, column=1)
 
 #delete key/value from dict
@@ -156,7 +202,7 @@ delete_entry.grid(row=5, column=1, sticky=N)
 delete_button = Button(window, text="Delete Word", bg="#FF6961", command=delete_key_value)
 delete_button.grid(row=5, column=1, pady=3)
 
-delete_status_label = Label(window, text="", fg="red")
+delete_status_label = Label(window, text="")
 delete_status_label.grid(row=5, column=1, sticky=S)
 
 window.mainloop()
